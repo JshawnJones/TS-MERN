b155260f4a9d02ee5603a59915cacee6b80d36a5
JshawnJones <ken.jutsu@hotmail.com>
2023-11-01 11:26:36 +0900

    Video-05-List-Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './Data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +9,21 @@ function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <header></header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
   )
